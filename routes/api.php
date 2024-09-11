5d5c3a5bfeb56ad2b526969c57a440a08b5a6900
Danang Ari Murti Wibowo <61486311+danangamw@users.noreply.github.com>
2024-09-11 15:17:03 +0700

    Added: api documentation with laravel scribe

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
     // Author routes
